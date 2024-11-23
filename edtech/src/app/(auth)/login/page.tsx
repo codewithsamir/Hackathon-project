bf80a4c886e4b3a7088271bd70ae5e27a2c63da6
Rohit3216 <158135615+Rohit3216@users.noreply.github.com>
2024-11-23 16:00:49 +0545

    header

---
+++ edtech/src/app/(auth)/login/page.tsx
@@ -0,0 +1,18 @@
+
+import Headers from "@/components/Header";
+
+const page = () => {
+  return (
+
+    // <section className='bg-red-500 w-32 h-32'>
+<Headers />
+
+
+    // </section>
+
+
+    
+  )
+}
+
+export default page;
---
+++ edtech/src/components/Section.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <section>
+        <div>
+            <div className='w-52 h-52 border-2 border-red-500'>
+                <h1>
+                    image
+                </h1>
+
+            </div>
+
+            <div>
+
+            </div>
+             
+             <div>
+
+             </div>
+
+        </div>
+
+    </section>
+
+
+
+
+  )
+}
+
+export default Section;
