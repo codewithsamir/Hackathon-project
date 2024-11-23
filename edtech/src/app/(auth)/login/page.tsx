cf8c9001691ebef5da82155a76b5f240bcc4cc83
partivashani <parisahani043@gmail.com>
2024-11-23 01:39:56 -0800

    This is first commit

---
+++ edtech/src/app/(auth)/login/page.tsx
@@ -0,0 +1,14 @@
+
+import Footer from '@/components/Footer'
+// import Signup from '@/components/Signup'
+import React from 'react'
+
+ const page = () => {
+  return (
+    <Footer/>
+    // <Signup/>
+  )
+}
+
+
+export default page;
